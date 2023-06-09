538d2c79da22ccce71e3739607f846d788801dda
Fernando Chazarreta <fernandochaza@gmail.com>
2023-06-09 08:24:05 -0300

    feat: Add hide/show menu behavior

--- scripts/responsiveMenu.js
+++ scripts/responsiveMenu.js
@@ -2,8 +2,18 @@
 function toggleMenu() {
     let menu = document.getElementById("my-top-nav");
     if (menu.className === "top-nav") {
-      menu.className += " responsive";
+      menu.className += " clicked";
+      document.querySelector(".main-title").textContent = "Menu";
     } else {
       menu.className = "top-nav";
+      document.querySelector(".main-title").textContent = "I am Groot";
+
+    }
+
+    let header = document.getElementById("my-header");
+    if (header.className === "header-nav") {
+      header.className += " clicked";
+    } else {
+      header.className = "header-nav";
     }
   }
